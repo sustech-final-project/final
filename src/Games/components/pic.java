cd88b77afb3cb58e42be8b55516e9e90723e7c7d
ntdgy <ntdgy2001@gmail.com>
2021-05-23 17:56:20 +0800

    5.23

--- src/Games/components/pic.java
+++ src/Games/components/pic.java
@@ -3,5 +3,19 @@ package Games.components;
 import javax.swing.*;
 
 public class pic {
-    final ImageIcon num1 = new ImageIcon("src\\Games\\pic\\test2.png");
+    final ImageIcon num1 = new ImageIcon("src\\Games\\image\\1.png");
+    final ImageIcon num2 = new ImageIcon("src\\Games\\image\\2.png");
+    final ImageIcon num3 = new ImageIcon("src\\Games\\image\\3.png");
+    final ImageIcon num4 = new ImageIcon("src\\Games\\image\\4.png");
+    final ImageIcon num5 = new ImageIcon("src\\Games\\image\\5.png");
+    final ImageIcon num6 = new ImageIcon("src\\Games\\image\\6.png");
+    final ImageIcon num7 = new ImageIcon("src\\Games\\image\\7.png");
+    final ImageIcon tnt = new ImageIcon("src\\Games\\image\\tnt.png");
+    final ImageIcon Chaqi = new ImageIcon("src\\Games\\image\\方块插旗.png");
+    final ImageIcon block = new ImageIcon("src\\Games\\image\\正常方块.png");
+    final ImageIcon blank = new ImageIcon("src\\Games\\image\\空白.png");
+    final ImageIcon Xuanfu = new ImageIcon("src\\Games\\image\\上方.png");
+
+
+
 }
