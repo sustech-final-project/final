9d209a4471dd39403d5b0147fc52c8a1c5ebf632
ntdgy <ntdgy2001@gmail.com>
2021-05-25 21:49:30 +0800

    5.25

--- src/Games/Map/test2.java
+++ src/Games/Map/test2.java
@@ -7,9 +7,9 @@ import javax.swing.*;
 public class test2 extends JFrame {
     public static void main(String[] args) throws InterruptedException {
         JPanel timerPanel = new SoloTimer(new JPanel()).getPanel1();
-        JPanel left = new JPanel(new AfYLayout());
-        left.add(timerPanel);
-        Thread.sleep(2000);
+        //JPanel left = new JPanel(new AfYLayout());
+        //left.add(timerPanel);
+        Thread.sleep(2001);
         System.out.println(SoloTimer.gettime());
 
     }
